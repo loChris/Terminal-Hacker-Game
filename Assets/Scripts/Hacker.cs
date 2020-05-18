2e8e5a8bdcf8a473dcf7edb834e7703caf262103
loChris <44450518+loChris@users.noreply.github.com>
2020-05-18 12:25:37 -0400

    added a main menu for choosing difficulty (still not functional)

---
+++ Assets/Scripts/Hacker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    void Start()
+    { 
+        ShowMainMenu("Hello Chris");
+    }
+
+    void Update()
+    {
+    }
+
+    private void ShowMainMenu(string greeting)
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack into?\n");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA\n");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    private void OnUserInput(string input)
+    {
+        print("The User typed: " + input);
+    }
+}
